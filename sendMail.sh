d6e5ff6acc13803add497a131b5fa2717d867d33
sergiornelas <42797252+sergiornelas@users.noreply.github.com>
2020-02-17 11:49:39 -0600

    Update sendMail.sh

--- sendMail.sh
+++ sendMail.sh
@@ -2,3 +2,6 @@
 
 mail -s "Metricas semanales" -a resultados.csv correo@hotmail.com < ./resultados.csv
 mail -s "Metricas team" -a resultados.csv correo@hotmail.com < ./team.csv
+
+#Enviar files desde la computadora local a un servidor:
+#   scp APAR* sergio@servidor.com:/home/sergio/metricas/
